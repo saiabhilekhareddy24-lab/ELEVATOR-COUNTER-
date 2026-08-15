1b6ebceea610b605773b089cad493785445c86d5
saiabhilekhareddy24-lab <saiabhilekhareddy24@gmail.com>
2026-08-15 21:42:33 +0530

    Create ElevatorCounterTestresult.java

---
+++ ElevatorCounterTestresult.java
@@ -0,0 +1,17 @@
+=================================
+      ELEVATOR COUNTER TEST
+=================================
+Person enters  -> Count: 1
+Person enters  -> Count: 2
+Person enters  -> Count: 3
+Person enters  -> Count: 4
+Person enters  -> Count: 5
+Person enters  -> DENIED (Elevator FULL)
+Person exits   -> Count: 4
+Person exits   -> Count: 3
+---------------------------------
+Current Count  : 3
+Maximum Capacity: 5
+Elevator Full  : false
+Elevator Empty : false
+=================================
